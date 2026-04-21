29ccd6a351278e03bce401c5cb8d37f3431f17a4
A1VEN23 <120800456+A1VEN23@users.noreply.github.com>
2026-04-21 21:45:02 +0300

    Delete ботик directory

--- ботик/kill_bot.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Stopping all Python bot instances...
-taskkill /F /IM python.exe /T 2>nul
-taskkill /F /IM python3.exe /T 2>nul
-echo Done! Wait 5 seconds before starting bot again.
-timeout /t 5
